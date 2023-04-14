ef67cff7c2fa52de85c9b0080db758b5678f94a9
sarah-zhan <yizh.contact@gmail.com>
2023-04-14 10:46:29 -0700

    re-save orderSlice

--- frontend/src/features/order/orderSlice.js
+++ frontend/src/features/order/orderSlice.js
@@ -2,17 +2,16 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 // import orderItems from '../../orderItems'  //cannot import, need to check
 const initialState = {
-  // orderItems: orderItems,
-  orderItems: [],
-  amount: 0,
-  total: 0,
-  isLoading: true,
+	// orderItems: orderItems,
+	orderItems: [],
+	amount: 0,
+	total: 0,
+	isLoading: true,
 };
 
 const orderSlice = createSlice({
-  name: 'order',
-  initialState
-
+	name: 'order',
+	initialState,
 });
 
 // console.log(orderSlice);
