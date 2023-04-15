99e32c4b174b7bb7637591eb6a0f6b548dda4384
sarah-zhan <yizh.contact@gmail.com>
2023-04-15 12:18:13 -0700

    add pagination

--- frontend/src/features/order/OrderProductList.jsx
+++ frontend/src/features/order/OrderProductList.jsx
@@ -1,84 +1,9 @@
 import OrderProduct from './OrderProduct';
 import { useEffect, useState } from 'react';
-import { Space, Switch, Table, Popconfirm } from 'antd';
+import { Space, Switch, Table, Popconfirm, Button } from 'antd';
 import axios from 'axios';
 import orderProducts from '../../../orderProducts';
-const columns = [
-	{
-		title: 'Order_id',
-		dataIndex: 'entity_id',
-		key: 'entity_id',
-	},
-	{
-		title: 'Created_Date',
-		dataIndex: 'created_at',
-	},
-	{
-		title: 'Email',
-		dataIndex: 'customer_email',
-		editTable: true,
-	},
-	{
-		title: 'First Name',
-		dataIndex: 'customer_firstname',
-	},
-	{
-		title: 'Last Name',
-		dataIndex: 'customer_lastname',
-	},
-	{
-		title: 'Total',
-		dataIndex: 'grand_total',
-		editTable: true,
-	},
-	{
-		title: 'Increment_id',
-		dataIndex: 'increment_id',
-		key: 'increment_id',
-	},
-	{
-		title: 'Total Qty',
-		dataIndex: 'total_qty_ordered',
-		editTable: true,
-	},
-	{
-		title: 'Action',
-		dataIndex: 'action',
-		render: (_, record) => orderProducts.length >= 1? (
-		<Popconfirm title='Are you sure you want to delete?' onConfirm={() => handleDelete(record)}>
 
-		</Popconfirm>
-		)
-	},
-];
-
-const children = [
-	{
-		title: 'ID',
-		dataIndex: 'id',
-		key: 'id',
-	},
-	{
-		title: 'Item',
-		dataIndex: 'name',
-	},
-	{
-		title: 'SKU',
-		dataIndex: 'sku',
-	},
-	{
-		title: 'Price',
-		dataIndex: 'price',
-	},
-	{
-		title: 'Product_id',
-		dataIndex: 'product_id',
-	},
-	{
-		title: 'Qty',
-		dataIndex: 'qty_ordered',
-	},
-];
 const rowSelection = {
 	onChange: (selectedRowKeys, selectedRows) => {
 		console.log(
@@ -92,6 +17,18 @@ const rowSelection = {
 	},
 	onSelectAll: (selected, selectedRows, changeRows) => {
 		console.log(selected, selectedRows, changeRows);
+	},
+};
+
+const pagination = {
+	total: data.length,
+	current: 1,
+	showSizeChanger: true,
+	onShowSizeChange(current, pageSize) {
+		console.log('Current: ', current, '; PageSize: ', pageSize);
+	},
+	onChange(current) {
+		console.log('Current: ', current);
 	},
 };
 
@@ -114,18 +51,105 @@ const OrderProductList = () => {
 	// 	...orders,
 	// 	children: items,
 	// }));
-	// console.log();
 
+	const handleDelete = value => {
+		const dataSource = [...orderProducts]; //modifiedData
+		const filteredOrders = dataSource.filter(
+			order => order.entity_id !== value.id
+		);
+		setOrders(filteredOrders);
+	};
+
+	const columns = [
+		{
+			title: 'Order_id',
+			dataIndex: 'entity_id',
+			key: 'entity_id',
+		},
+		{
+			title: 'Created_Date',
+			dataIndex: 'created_at',
+		},
+		{
+			title: 'Email',
+			dataIndex: 'customer_email',
+			editTable: true,
+		},
+		{
+			title: 'First Name',
+			dataIndex: 'customer_firstname',
+		},
+		{
+			title: 'Last Name',
+			dataIndex: 'customer_lastname',
+		},
+		{
+			title: 'Total',
+			dataIndex: 'grand_total',
+			editTable: true,
+		},
+		{
+			title: 'Increment_id',
+			dataIndex: 'increment_id',
+			key: 'increment_id',
+		},
+		{
+			title: 'Total Qty',
+			dataIndex: 'total_qty_ordered',
+			editTable: true,
+		},
+		{
+			title: 'Action',
+			dataIndex: 'action', //modefiedData below
+			render: (_, record) =>
+				orderProducts.length >= 1 ? (
+					<Popconfirm
+						title='Are you sure you want to delete?'
+						onConfirm={() => handleDelete(record)}
+					>
+						<Button danger type='primary'>
+							Delete
+						</Button>
+					</Popconfirm>
+				) : null,
+		},
+	];
+
+	const children = [
+		{
+			title: 'ID',
+			dataIndex: 'id',
+			key: 'id',
+		},
+		{
+			title: 'Item',
+			dataIndex: 'name',
+		},
+		{
+			title: 'SKU',
+			dataIndex: 'sku',
+		},
+		{
+			title: 'Price',
+			dataIndex: 'price',
+		},
+		{
+			title: 'Product_id',
+			dataIndex: 'product_id',
+		},
+		{
+			title: 'Qty',
+			dataIndex: 'qty_ordered',
+		},
+	];
 	return (
 		<>
-
 			<Table
 				columns={columns}
 				childrenColumnName={children}
-				rowSelection={{
-					...rowSelection,
-				}}
-				dataSource={orderProducts}
+				rowSelection={rowSelection}
+				pagination={pagination}
+				dataSource={orderProducts} //modefiedData
 				bordered
 				loading={loading}
 			/>
