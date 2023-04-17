6f5fb64cd4160510ca357c61b5620d0e002a837a
sarah-zhan <yizh.contact@gmail.com>
2023-04-16 21:42:36 -0700

    change to use ordertable

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 		<main>
 			<Navbar />
 			{/* <Testing /> */}
+			{/* <OrderProductList /> */}
 			<OrderTable />
 		</main>
 	);
--- frontend/src/features/order/OrderTable.jsx
+++ frontend/src/features/order/OrderTable.jsx
@@ -1,16 +1,8 @@
 import { DownOutlined } from '@ant-design/icons';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Badge, Dropdown, Space, Table } from 'antd';
 import orderProducts from '../../../orderProducts';
-const items = [
-	{
-		key: '1',
-		label: 'Action 1',
-	},
-	{
-		key: '2',
-		label: 'Action 2',
-	},
-];
 
 const sampleData = [
 	{
@@ -125,8 +117,45 @@ const sampleData = [
 	},
 ];
 
-console.log('sampleLength', sampleData.length);
 const OrderTable = () => {
+	const [orders, setOrders] = useState([]);
+	const [sortedInfo, setSortedInfo] = useState({});
+	const [loading, setLoading] = useState(false);
+	useEffect(() => {
+		loadData();
+	}, []);
+
+	const loadData = async () => {
+		setLoading(true);
+		const response = await axios.get('orderProductsJoin.json');
+		console.log('response: ', response);
+
+		setOrders(response.data);
+		setLoading(false);
+	};
+
+	const prepareData = arr => {
+		if (arr.length < 1) {
+			return [];
+		}
+		return arr.map(order => ({ ...order, children: order.items }));
+	};
+
+	const modifiedData = prepareData(orders);
+
+	const handleDelete = value => {
+		const dataSource = [...modifiedData]; //modifiedData
+		const filteredOrders = dataSource.filter(
+			order => order.entity_id !== value.id
+		);
+		setOrders(filteredOrders);
+	};
+
+	const handleChange = (...sorter) => {
+		const { order, field } = sorter[2];
+		setSortedInfo({ columnKey: field, order });
+	};
+
 	const expandedRowRender = () => {
 		const columns = [
 			{
@@ -170,19 +199,25 @@ const OrderTable = () => {
 					</Space>
 				),
 			},
+			{
+				title: 'Order_id',
+				dataIndex: 'order_id',
+				key: 'order_id',
+			},
 		];
 
 		const data = [];
-		for (let i = 0; i < sampleData.length; i++) {
-			console.log('sampleData[i].name: ', sampleData[i]);
+		for (let i = 0; i < modifiedData.length; i++) {
+			console.log('modifiedData[i].name: ', modifiedData[i]);
 			data.push({
 				key: i.toString(),
 				id: i + 1,
-				name: sampleData[i].items[i].name,
-				sku: sampleData[i].items[i].sku,
-				price: sampleData[i].items[i].price,
-				product_id: sampleData[i].items[i].product_id,
-				qty_ordered: sampleData[i].items[i].qty_ordered,
+				name: modifiedData[i]?.items[i]?.name,
+				sku: modifiedData[i]?.items[i]?.sku,
+				price: modifiedData[i]?.items[i]?.price,
+				product_id: modifiedData[i]?.items[i]?.product_id,
+				qty_ordered: modifiedData[i]?.items[i]?.qty_ordered,
+				order_id: modifiedData[i]?.items[i]?.order_id,
 			});
 		}
 
@@ -194,64 +229,64 @@ const OrderTable = () => {
 			title: 'Order_id',
 			dataIndex: 'entity_id',
 			key: 'entity_id',
-			// sorter: (a, b) => a.entity_id - b.entity_id,
-			// sortOrder: sortedInfo.columnKey === 'entity_id' && sortedInfo.order,
+			sorter: (a, b) => a.entity_id - b.entity_id,
+			sortOrder: sortedInfo.columnKey === 'entity_id' && sortedInfo.order,
 		},
 		{
 			title: 'Created_Date',
 			dataIndex: 'created_at',
 			key: 'created_at',
-			// sorter: (a, b) => a.created_at?.localeCompare(b.created_at),
-			// sortOrder: sortedInfo.columnKey === 'created_at' && sortedInfo.order,
+			sorter: (a, b) => a.created_at?.localeCompare(b.created_at),
+			sortOrder: sortedInfo.columnKey === 'created_at' && sortedInfo.order,
 		},
 		{
 			title: 'Email',
 			dataIndex: 'customer_email',
 			key: 'customer_email',
 			editTable: true,
-			// sorter: (a, b) => a.customer_email?.localeCompare(b.customer_email),
-			// sortOrder: sortedInfo.columnKey === 'customer_mail' && sortedInfo.order,
+			sorter: (a, b) => a.customer_email?.localeCompare(b.customer_email),
+			sortOrder: sortedInfo.columnKey === 'customer_mail' && sortedInfo.order,
 		},
 		{
 			title: 'First Name',
 			dataIndex: 'customer_firstname',
 			key: 'customer_firstname',
-			// sorter: (a, b) =>
-			// 	a.customer_firstname?.localeCompare(b.customer_firstname),
-			// sortOrder:
-			// 	sortedInfo.columnKey === 'customer_firstname' && sortedInfo.order,
+			sorter: (a, b) =>
+				a.customer_firstname?.localeCompare(b.customer_firstname),
+			sortOrder:
+				sortedInfo.columnKey === 'customer_firstname' && sortedInfo.order,
 		},
 		{
 			title: 'Last Name',
 			dataIndex: 'customer_lastname',
 			key: 'customer_lastname',
-			// sorter: (a, b) => a.customer_lastname?.localeCompare(b.customer_lastname),
-			// sortOrder:
-			// 	sortedInfo.columnKey === 'customer_lastname' && sortedInfo.order,
+			sorter: (a, b) => a.customer_lastname?.localeCompare(b.customer_lastname),
+			sortOrder:
+				sortedInfo.columnKey === 'customer_lastname' && sortedInfo.order,
 		},
 		{
 			title: 'Total',
 			dataIndex: 'grand_total',
 			key: 'grand_total',
 			editTable: true,
-			// sorter: (a, b) => a.grand_total - b.grand_total,
-			// sortOrder: sortedInfo.columnKey === 'grand_total' && sortedInfo.order,
+			sorter: (a, b) => a.grand_total - b.grand_total,
+			sortOrder: sortedInfo.columnKey === 'grand_total' && sortedInfo.order,
 		},
 		{
 			title: 'Increment_id',
 			dataIndex: 'increment_id',
 			key: 'increment_id',
-			// sorter: (a, b) => a.increment_id - b.increment_id,
-			// sortOrder: sortedInfo.columnKey === 'increment_id' && sortedInfo.order,
+			sorter: (a, b) => a.increment_id - b.increment_id,
+			sortOrder: sortedInfo.columnKey === 'increment_id' && sortedInfo.order,
 		},
 		{
 			title: 'Total Qty',
 			dataIndex: 'total_qty_ordered',
 			key: 'total_qty_ordered',
 			editTable: true,
-			// sorter: (a, b) => a.total_qty_ordered - b.total_qty_ordered,
-			// sortOrder:
-			// 	sortedInfo.columnKey === 'total_qty_ordered' && sortedInfo.order,
+			sorter: (a, b) => a.total_qty_ordered - b.total_qty_ordered,
+			sortOrder:
+				sortedInfo.columnKey === 'total_qty_ordered' && sortedInfo.order,
 		},
 		{
 			title: 'Action',
@@ -265,7 +300,7 @@ const OrderTable = () => {
 		},
 	];
 
-	const data = sampleData.map(order => ({
+	const data = modifiedData.map(order => ({
 		key: order.entity_id.toString(),
 		...order,
 	}));
@@ -279,6 +314,9 @@ const OrderTable = () => {
 					defaultExpandedRowKeys: ['0'],
 				}}
 				dataSource={data}
+				bordered
+				rowKey={record => record.id}
+				onChange={handleChange}
 				size='middle'
 			/>
 		</>
