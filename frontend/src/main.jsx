5775783399bd5fb15c303ce5406b19679ea49199
sarah-zhan <yizh.contact@gmail.com>
2023-04-17 11:39:27 -0700

    add all routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,12 @@ import App from './App';
 import './index.css';
 import './scss/styles.scss';
 import * as bootstrap from 'bootstrap';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 );
