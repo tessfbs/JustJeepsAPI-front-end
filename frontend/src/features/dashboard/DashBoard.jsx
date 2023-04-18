d514b784ff0722c9711af7e32ba9b268ebf8e178
roylyh <roy.louyihui@gmail.com>
2023-04-18 11:41:17 -0700

    dashboard order page

--- frontend/src/features/dashboard/DashBoard.jsx
+++ frontend/src/features/dashboard/DashBoard.jsx
@@ -1,6 +1,9 @@
 import "./dashboard.scss";
 import Sidebar from "../sidebar/Sidebar";
 import Widget from "../widget/Widget";
+import Featured from "../featured/Featured";
+import Chart from "../chart/Chart";
+import List from "../list/List";
 
 export const DashBoard = () => {
   return (
@@ -13,12 +16,14 @@ export const DashBoard = () => {
           <Widget type="aveordervalue" />
           <Widget type="totalqtyorder" />
         </div>
-				<div className="charts">
-					charts
-				</div>
-				<div className="listContainer">
-					List
-				</div>
+        <div className="charts">
+          <Featured />
+          <Chart aspect={3 / 1} title="This year (Revenue)" />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Order Status</div>
+          <List />
+        </div>
       </div>
     </div>
   );
