7fe12d8e7289cee60f819ca8704fa50481fbbce2
roylyh <roy.louyihui@gmail.com>
2023-04-18 15:43:39 -0700

    initialize dashboard products

---
+++ frontend/src/features/dashboard/DashBoardPO.jsx
@@ -0,0 +1,59 @@
+import "./dashboardpo.scss";
+import Sidebar from "../sidebar/Sidebar";
+import Widget from "../widget/Widget";
+import Piechart from "../pie/Piechart";
+
+export const DashBoardPO = () => {
+  return (
+    <div className="dashboard">
+      <Sidebar />
+      <div className="dashboardContainer">
+        <div className="left">
+          <div className="widgets">
+            <Widget type="totalorders" />
+            <Widget type="totalrevenue" />
+          </div>
+
+          <div className="mostPopular">
+            <h1 className="title">Most Popular Product</h1>
+            <div className="item">
+              <img
+                src="https://www.justjeeps.com/pub/media/catalog/product//0/1/01-046.jpg"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">
+                  LUK Clutch Kit For 2007-11 JK Wrangler 3.8 Ltr & 2002-04
+                  Liberty KJ 3.7 Ltr
+                </h1>
+                <div className="detailItem">
+                  <span className="itemKey">SKU:</span>
+                  <span className="itemValue">01-046</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Wight:</span>
+                  <span className="itemValue">4</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Total Ordered This Year:</span>
+                  <span className="itemValue">28</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Vendor:</span>
+                  <span className="itemValue">Meyer</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div className="rightContainer">
+            <h1 className="title">Orders By Vendors</h1>
+            <Piechart />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/features/pie/Piechart.jsx
@@ -0,0 +1,74 @@
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+import "./piechart.scss";
+
+const Piechart = () => {
+  const data = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+    { name: "Group D", value: 200 },
+  ];
+
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  return (
+    <div className="piechart">
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
+        <PieChart width={800} height={800}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={150}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+      <div className="bottom">
+        <div className="colorOption optionA">Keystone</div>
+        <div className="colorOption optionB">Meyer</div>
+        <div className="colorOption optionC">Omix</div>
+        <div className="colorOption optionD">Quadratec</div>
+      </div>
+    </div>
+  );
+};
+
+export default Piechart;
