21d0bcbfcb66ee8cfa4193e74556563cbaf8bbf7
sarah-zhan <yizh.contact@gmail.com>
2023-04-18 23:00:44 -0700

    remove unused file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import OrdersList from './features/order/OrdersList.jsx';
 import OrderProductList from './features/order/OrderProductList.jsx';
 import OrderTable from './features/order/OrderTable.jsx';
-import Testing from './features/order/testing.jsx';
 import { Route, Routes } from 'react-router-dom';
 import { SupplierTable } from './features/supplier/SupplierTable.jsx';
 import { DashBoard } from './features/dashboard/DashBoard.jsx';
@@ -13,23 +12,22 @@ import { PoForm } from './features/po/PoForm.jsx';
 import { Items } from './features/items/Items.jsx';
 
 function App() {
+	const [data, setData] = useState('');
 
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-    const fetchDataFromBackend = async () => {
-      try {
-        const response = await axios.get('http://localhost:8080/api/data'); // Use Axios to send GET request to /api/data
-        const responseData = response.data;
-        // console.log('Data from backend:', responseData);
-        // Process the response data from backend if needed
-        setData(responseData);
-      } catch (error) {
-        console.error('Failed to fetch data from backend:', error);
-      }
-    };
-    fetchDataFromBackend();
-  }, []);
+	useEffect(() => {
+		const fetchDataFromBackend = async () => {
+			try {
+				const response = await axios.get('http://localhost:8080/api/data'); // Use Axios to send GET request to /api/data
+				const responseData = response.data;
+				// console.log('Data from backend:', responseData);
+				// Process the response data from backend if needed
+				setData(responseData);
+			} catch (error) {
+				console.error('Failed to fetch data from backend:', error);
+			}
+		};
+		fetchDataFromBackend();
+	}, []);
 
 	return (
 		<>
@@ -39,10 +37,10 @@ function App() {
 				<Route path='/orders' element={<OrderTable />} />
 				<Route path='/suppliers' element={<SupplierTable />} />
 				{/* <Route path='/dashboard' element={<DashBoard />} /> */}
-        <Route path='/dashboard'>
-          <Route index element={<DashBoard/>}/>
-          <Route path='po' element={<DashBoardPO/>}/>
-        </Route>
+				<Route path='/dashboard'>
+					<Route index element={<DashBoard />} />
+					<Route path='po' element={<DashBoardPO />} />
+				</Route>
 				<Route path='/po' element={<PoForm />} />
 				<Route path='/items' element={<Items />} />
 			</Routes>
