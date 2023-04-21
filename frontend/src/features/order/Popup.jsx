acfa86864aec7d16c3ae753d8fc2fe352927ef5a
sarah-zhan <yizh.contact@gmail.com>
2023-04-20 20:52:11 -0700

    make the text automatically show in the pop up

---
+++ frontend/src/features/order/Popup.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect, useCallback } from 'react';
+import { Space, Input, Drawer } from 'antd';
+import { ClearOutlined, CoffeeOutlined } from '@ant-design/icons';
+import ProductTable from '../items/ProductTable';
+import axios from 'axios';
+
+const Popup = ({ placement, onClose, sku }) => {
+	const [searchTermSku, setSearchTermSku] = useState(sku);
+	const [dataProduct, setDataProduct] = useState([]);
+
+	const resetDrawer = useCallback(() => {
+		setSearchTermSku('');
+		setDataProduct([]);
+	}, []);
+
+	const getProductBySku = useCallback(searchTermSku => {
+		try {
+			if (searchTermSku) {
+				// Add null check
+				return axios
+					.get(`http://localhost:8080/api/products/${searchTermSku}`)
+					.then(res => {
+						const responseData = res.data;
+						// Process the response data from backend if needed
+						setDataProduct([responseData]);
+					});
+			}
+		} catch (error) {
+			console.error('Failed to fetch data from backend:', error);
+		}
+	}, []);
+
+	useEffect(() => {
+		setSearchTermSku(sku);
+		getProductBySku(sku);
+	}, [sku]);
+
+	return (
+		<Drawer placement={placement} width={300} onClose={onClose} open={true}>
+			<div className='mb-2'>
+				<Space>
+					<Input
+						placeholder='Enter your item'
+						onChange={setSearchTermSku}
+						type='text'
+						allowClear
+						value={searchTermSku}
+					/>
+					<CoffeeOutlined
+						size='middle'
+						style={{
+							width: 50,
+							color: 'black',
+						}}
+						onClick={() => getProductBySku(searchTermSku)}
+					/>
+					<ClearOutlined
+						size='middle'
+						style={{
+							color: 'black',
+						}}
+						onClick={() => {
+							resetDrawer();
+						}}
+					/>
+				</Space>
+			</div>
+			<div>
+				<ProductTable searchTermSku={searchTermSku} data={dataProduct} />
+			</div>
+		</Drawer>
+	);
+};
+export default Popup;
