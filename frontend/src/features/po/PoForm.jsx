f5242bfab55ddbc3be4b208eeade78bd8af7045f
sarah-zhan <yizh.contact@gmail.com>
2023-04-22 19:07:37 -0700

    add logos to po page

--- frontend/src/features/po/PoForm.jsx
+++ frontend/src/features/po/PoForm.jsx
@@ -1,94 +1,103 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import { Layout, Menu, Button, theme } from "antd";
-import PoTableTest from "./PoTableTest";
-
+	MenuFoldOutlined,
+	MenuUnfoldOutlined,
+	UploadOutlined,
+	UserOutlined,
+	VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme } from 'antd';
+import PoTableTest from './PoTableTest';
+import keystone from '../../assets/keystone.png';
+import meyer from '../../assets/meyer.png';
+import omix from '../../assets/omix.png';
 const { Header, Sider, Content } = Layout;
 
 export const PoForm = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const [currentNav, setCurrentNav] = useState("nav1");
+	const [collapsed, setCollapsed] = useState(false);
+	const [currentNav, setCurrentNav] = useState('nav1');
 
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+	const {
+		token: { colorBgContainer },
+	} = theme.useToken();
 
-  const handleNavClick = (e) => {
-    setCurrentNav(e.key);
-  };
+	const handleNavClick = e => {
+		setCurrentNav(e.key);
+	};
 
-  return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          items={[
-            {
-              key: "nav1",
-              icon: <VideoCameraOutlined />,
-              label: "Keystone",
-            },
-            {
-              key: "nav2",
-              icon: <UserOutlined />,
-              label: "Meyer",
-            },
-            {
-              key: "nav3",
-              icon: <UploadOutlined />,
-              label: "Omix-Ada",
-            },
-          ]}
-          onSelect={handleNavClick}
-        />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: "16px",
-              width: 64,
-              height: 64,
-            }}
-          />{" "}
-          PURCHASE ORDERS DETAILS
-        </Header>
-        <Content
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-          }}
-        >
-          {currentNav === "nav1" ? (
-            <div>
-              <PoTableTest vendorId="1" for Keystone />
-            </div>
-          ) : currentNav === "nav2" ? (
-            <div>
-              <PoTableTest vendorId="2" for Meyer />
-            </div>
-          ) : currentNav === "nav3" ? (
-            <div>
-              <PoTableTest vendorId="3" for Omix />
-            </div>
-          ) : null}
-        </Content>
-      </Layout>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Sider
+				trigger={null}
+				collapsible
+				collapsed={collapsed}
+				style={{ backgroundColor: '#2938c3' }}
+			>
+				<div className='logo' />
+				<Menu
+					mode='inline'
+					defaultSelectedKeys={['1']}
+					style={{ backgroundColor: '#2938c3', color: '#D4F1F4' }}
+					items={[
+						{
+							key: 'nav1',
+							icon: <img src={keystone} alt='keystone' width='80px' />,
+						},
+						{
+							key: 'nav2',
+							icon: <img src={meyer} alt='meyer' width='80px' />,
+						},
+						{
+							key: 'nav3',
+							icon: <img src={omix} alt='omix' width='80px' />,
+						},
+						{
+							key: 'nav4',
+							icon: <UploadOutlined />,
+							label: 'Quadratec',
+						},
+					]}
+					onSelect={handleNavClick}
+				/>
+			</Sider>
+			<Layout>
+				<Header style={{ padding: 0, background: colorBgContainer }}>
+					<Button
+						type='text'
+						icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+						onClick={() => setCollapsed(!collapsed)}
+						style={{
+							fontSize: '16px',
+							width: 64,
+							height: 64,
+						}}
+					/>{' '}
+					PURCHASE ORDERS DETAILS
+				</Header>
+				<Content
+					style={{
+						margin: '24px 16px',
+						padding: 24,
+						minHeight: 280,
+						background: colorBgContainer,
+					}}
+				>
+					{currentNav === 'nav1' ? (
+						<div>
+							<PoTableTest vendorId='1' for Keystone />
+						</div>
+					) : currentNav === 'nav2' ? (
+						<div>
+							<PoTableTest vendorId='2' for Meyer />
+						</div>
+					) : currentNav === 'nav3' ? (
+						<div>
+							<PoTableTest vendorId='3' for Omix />
+						</div>
+					) : null}
+				</Content>
+			</Layout>
+		</Layout>
+	);
 };
