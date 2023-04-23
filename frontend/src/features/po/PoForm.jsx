abb37181afcc05f47c3ec46f1c2a0fa9d6a323c4
sarah-zhan <yizh.contact@gmail.com>
2023-04-22 20:23:03 -0700

    change color of side bar

--- frontend/src/features/po/PoForm.jsx
+++ frontend/src/features/po/PoForm.jsx
@@ -1,17 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {
-	MenuFoldOutlined,
-	MenuUnfoldOutlined,
-	UploadOutlined,
-	UserOutlined,
-	VideoCameraOutlined,
-} from '@ant-design/icons';
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import { Layout, Menu, Button, theme } from 'antd';
 import PoTableTest from './PoTableTest';
 import keystone from '../../assets/keystone.png';
 import meyer from '../../assets/meyer.png';
 import omix from '../../assets/omix.png';
+import quadratec from '../../assets/quadratec.png';
 const { Header, Sider, Content } = Layout;
 
 export const PoForm = () => {
@@ -38,24 +33,28 @@ export const PoForm = () => {
 				<Menu
 					mode='inline'
 					defaultSelectedKeys={['1']}
-					style={{ backgroundColor: '#2938c3', color: '#D4F1F4' }}
+					style={{
+						backgroundColor: '#2938c3',
+						color: '#D4F1F4',
+						height: '15px',
+						marginTop: '80px',
+					}}
 					items={[
 						{
 							key: 'nav1',
-							icon: <img src={keystone} alt='keystone' width='80px' />,
+							icon: <img src={keystone} alt='keystone' width='100px' />,
 						},
 						{
 							key: 'nav2',
-							icon: <img src={meyer} alt='meyer' width='80px' />,
+							icon: <img src={meyer} alt='meyer' width='100px' />,
 						},
 						{
 							key: 'nav3',
-							icon: <img src={omix} alt='omix' width='80px' />,
+							icon: <img src={omix} alt='omix' width='100px' />,
 						},
 						{
 							key: 'nav4',
-							icon: <UploadOutlined />,
-							label: 'Quadratec',
+							icon: <img src={quadratec} alt='quadratec' width='100px' />,
 						},
 					]}
 					onSelect={handleNavClick}
@@ -78,8 +77,9 @@ export const PoForm = () => {
 				<Content
 					style={{
 						margin: '24px 16px',
+						fontFamily: 'Space Mono',
 						padding: 24,
-						minHeight: 280,
+						minHeight: 700,
 						background: colorBgContainer,
 					}}
 				>
